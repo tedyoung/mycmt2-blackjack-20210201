b67a4dcd29214d62a4ed88d4541f1dff63a3e3be
Ted M. Young <tedyoung@gmail.com>
2021-01-31 22:41:41 -0800

    Lab 2b: Moved code to ConsoleHand.

--- src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -20,7 +20,7 @@ public class HandDisplayTest {
     Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE),
                                  new Card(Suit.CLUBS, Rank.JACK)));
 
-    assertThat(hand.cardsAsString())
+    assertThat(ConsoleHand.cardsAsString(hand))
         .isEqualTo("\u001B[31m┌─────────┐\u001B[1B\u001B[11D│A        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        A│\u001B[1B\u001B[11D└─────────┘\u001B[6A\u001B[1C\u001B[30m┌─────────┐\u001B[1B\u001B[11D│J        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        J│\u001B[1B\u001B[11D└─────────┘");
   }
 
