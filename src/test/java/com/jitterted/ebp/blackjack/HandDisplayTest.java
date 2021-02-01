b3020030f97f7bb3b910808b7ebcfc2898d83b84
Ted M. Young <tedyoung@gmail.com>
2021-01-31 22:37:46 -0800

    Lab 2b: Created test for extracted cardsAsString method on Hand.

--- src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -14,4 +14,14 @@ public class HandDisplayTest {
     assertThat(ConsoleHand.displayFirstCard(hand))
         .isEqualTo("\u001B[31m┌─────────┐\u001B[1B\u001B[11D│A        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        A│\u001B[1B\u001B[11D└─────────┘");
   }
+
+  @Test
+  public void displayOfHandWithTwoCards() throws Exception {
+    Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE),
+                                 new Card(Suit.CLUBS, Rank.JACK)));
+
+    assertThat(hand.cardsAsString())
+        .isEqualTo("\u001B[31m┌─────────┐\u001B[1B\u001B[11D│A        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        A│\u001B[1B\u001B[11D└─────────┘\u001B[6A\u001B[1C\u001B[30m┌─────────┐\u001B[1B\u001B[11D│J        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♣    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        J│\u001B[1B\u001B[11D└─────────┘");
+  }
+
 }
