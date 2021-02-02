8aac61b9f5d9c30d2080071039cfaffb885b7013
Ted M. Young <tedyoung@gmail.com>
2021-02-01 19:22:43 -0800

    Day 2: Introduce ConsoleGame for starting the Console Adapter.

--- src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
+++ src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
@@ -6,7 +6,7 @@ import static org.fusesource.jansi.Ansi.ansi;
 
 public class ConsoleHand {
 
-  static String displayFirstCard(Hand hand) {
+  public static String displayFirstCard(Hand hand) {
     return ConsoleCard.display(hand.cards().get(0));
   }
 
